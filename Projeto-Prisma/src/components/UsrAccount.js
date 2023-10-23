a83ce00dc8ac9aec4557c51579c8c762901fbc1d
Elias Ribeiro <89391030+iaselrds@users.noreply.github.com>
2023-10-23 15:45:52 -0300

    Add files via upload

---
+++ Projeto-Prisma/src/components/UsrAccount.js
@@ -0,0 +1,13 @@
+import '../styles/components/UsrAccount.css';
+
+export default function UsrAccount(){
+    return(
+        <div className="usr-main">
+            <div className="name">
+                nomenomenome
+            </div>
+            <div className="picture">
+            </div>
+        </div>
+    )
+}
