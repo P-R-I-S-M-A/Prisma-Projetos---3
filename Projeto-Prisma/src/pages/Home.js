b07aa7880e9b4fe8431208fe74639fa4c73bfc2a
Elias Ribeiro <89391030+iaselrds@users.noreply.github.com>
2023-10-23 15:45:04 -0300

    Add files via upload

--- Projeto-Prisma/src/pages/Home.js
+++ Projeto-Prisma/src/pages/Home.js
@@ -3,19 +3,20 @@ import '../styles/pages/Home.css';
 export default function Home(){
     return(
         <div className="home">
-
-            <div className="mprojects">
-            </div>
-            <div className="blocks">
-                <div className="progress">
+            <div className="box">
+                <div className="mprojects">
                 </div>
-                <div className="notif">
+                <div className="blocks">
+                    <div className="progress">
+                    </div>
+                    <div className="notif">
+                    </div>
+                    <div className="bLeft">
+                    </div>
+                    <div className="bRight">
+                    </div>
                 </div>
-                <div className="bLeft">
-                </div>
-                <div className="bRight">
-                </div>
-            </div>
+            </div>    
         </div>
     )
 }
