6584444572af825c59a69a2a2054ff83d7138ce5
Elias Ribeiro <89391030+iaselrds@users.noreply.github.com>
2023-11-11 22:47:08 -0300

    Add files via upload

--- Projeto-Prisma/src/components/UsrAccount.js
+++ Projeto-Prisma/src/components/UsrAccount.js
@@ -1,9 +1,38 @@
 import '../styles/components/UsrAccount.css';
+import React, { useEffect, useState } from "react"; 
+import { collection, getDocs } from "firebase/firestore"; 
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from '../App';
 
 export default function UsrAccount(){
+
+    function mostrarNome(){
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (user !== null) {
+      const displayName = user.displayName
+      const email = user.email
+      const uid = user.uid
+      console.log(displayName)
+      console.log(email)
+      console.log(uid)
+      console.log(displayName)
+      document.getElementById('name').innerText = email
+    } else {
+      console.log("nao")
+    }
+}
+
+useEffect(() => { 
+    mostrarNome(); 
+  }, [])
+
+
     return(
         <div className="usr-main">
-            <div className="name">
+            <div id="name">
                 nomenomenome
             </div>
             <div className="picture">
